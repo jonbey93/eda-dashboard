8875d9b543e4442fae6d5e8b5fa8955a857166d6
jonbey93 <jonasbeyer93@gmail.com>
2025-05-18 13:28:25 +0200

    Remove test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from utils.llm import query_llm
-
-# Test the query_llm function
-prompt = "I havent uploaded any data yet. Can you show me a line plot of the x and y columns?"
-response = query_llm(prompt)
-print("Prompt:", prompt)
-print("Response:", response)
